44de6f688862ab9101cf38574606dfb0913e57c1
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-10 22:30:29 -0400

    add basic setup for navbar

--- src/components/nav/navDOM.js
+++ src/components/nav/navDOM.js
@@ -1,5 +1,26 @@
 const content = document.querySelector('#content');
 
 const loadNav = () => {
-    
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+
+    const navList = document.createElement('ul');
+
+    const home = document.createElement('li');
+    home.textContent = 'Home';
+
+    const projects = document.createElement('li');
+    projects.textContent = 'Projects';
+
+    content.appendChild(header);
+
+    header.appendChild(nav);
+
+    nav.appendChild(navList)
+
+    navList.appendChild(home);
+    navList.appendChild(projects)
+
 }
+
+export default loadNav
