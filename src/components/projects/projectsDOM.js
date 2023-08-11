2a800feab8febde27118d9aa60b2d59f23742e6a
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-11 02:19:54 -0400

    Add basic dom elements for projects page

---
+++ src/components/projects/projectsDOM.js
@@ -0,0 +1,12 @@
+const loadProjectsPage = () => {
+    const content = document.querySelector('#content');
+
+    const subtitle = document.createElement('h1');
+    subtitle.textContent = 'Projects'
+    content.appendChild(subtitle);
+
+
+
+}
+
+export default loadProjectsPage
