418c347344cc6270783c7a2653209a229a312445
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-11 20:58:12 -0400

    Add form and button to create a project

--- src/components/projects/projectsDOM.js
+++ src/components/projects/projectsDOM.js
@@ -1,12 +1,102 @@
+const content = document.querySelector('#content');
 const loadProjectsPage = () => {
-    const content = document.querySelector('#content');
+    
 
     const subtitle = document.createElement('h1');
-    subtitle.textContent = 'Projects'
+    subtitle.textContent = 'Projects';
     content.appendChild(subtitle);
 
+    content.appendChild(project());
+
+    content.appendChild(createProject());
+
+}
+
+const project = () => {
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('project');
+
+    // create logic to loop through available projects and display them
+
+    return projectDiv
+}
 
 
+const createProject = () => {
+    const createProject = document.createElement('button');
+    createProject.textContent = 'Create Project'
+    createProject.dataset.active = 'false';
+    createProject.classList.add('create-project');
+
+    
+
+    createProject.addEventListener('click', e => {
+        if (e.target.dataset.active === 'false') {
+            e.target.dataset.active = 'true'
+            createProjectForm()
+        } else {
+            e.target.dataset.active = 'false'
+            content.removeChild(content.lastChild)
+        }
+        
+    })
+
+    return createProject
+}
+
+
+const createProjectForm = () => {
+    // div and form elements
+    const projectFormDiv = document.createElement('div');
+    const projectForm = document.createElement('form');
+
+    projectFormDiv.classList.add('project-form');
+    projectFormDiv.dataset.active = 'false';
+
+    projectForm.setAttribute('action', '#');
+    projectForm.classList.add('add-project-form');
+    projectForm.setAttribute('id', 'project-form')
+
+    content.appendChild(projectFormDiv)
+    projectFormDiv.appendChild(projectForm);
+
+    // input/lable for title of project
+    const projectTitleInput = document.createElement('input');
+    const projectTitleLabel = document.createElement('label');
+
+    projectTitleInput.setAttribute('id', 'title');
+    projectTitleInput.setAttribute('type', 'text');
+    projectTitleInput.setAttribute('name', 'title');
+
+    projectTitleLabel.setAttribute('for', 'title');
+    projectTitleLabel.textContent = 'Project Name:'
+
+    projectForm.appendChild(projectTitleLabel);
+    projectForm.appendChild(projectTitleInput);
+
+    // description of project
+    const projectDescriptionInput = document.createElement('textarea');
+    const projectDescriptionlabel = document.createElement('label');
+
+    projectDescriptionInput.setAttribute('id', 'description');
+    projectDescriptionInput.setAttribute('rows', '4');
+    projectDescriptionInput.setAttribute('col', '50');
+    projectDescriptionInput.setAttribute('name', 'description');
+
+    projectDescriptionlabel.setAttribute('for', 'description');
+    projectDescriptionlabel.textContent = 'Project Description:'
+
+    projectForm.appendChild(projectDescriptionlabel);
+    projectForm.appendChild(projectDescriptionInput);
+    //add to-do for specific project
+
+
+    //submit button
+    const submit = document.createElement('button');
+    submit.textContent = 'Submit';
+    projectForm.appendChild(submit);
+
+    return projectFormDiv
 }
 
 export default loadProjectsPage
