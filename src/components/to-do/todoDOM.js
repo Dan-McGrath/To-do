b7ad44cd97aa4fa7f8bc8c8f39c2639ab4069dff
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-13 19:41:07 -0400

    create todo DOM info

---
+++ src/components/to-do/todoDOM.js
@@ -0,0 +1,42 @@
+const createTodo = () => {
+    const todoDiv = document.createElement('div');
+    const todo = document.createElement('p');
+
+}
+
+const todoForm = () => {
+    const projectDiv = document.querySelector('.project')
+
+    const todoFormDiv = document.createElement('div');
+    todoFormDiv.dataset.active = 'false';
+    todoFormDiv.classList.add('todo-form');
+
+    projectDiv.appendChild(todoFormDiv)
+
+    const todoForm = document.createElement('form');
+    todoForm.setAttribute('action', '#');
+    todoForm.setAttribute('class', 'add-todo-form')
+    todoForm.setAttribute('id', 'todo-form')
+
+    todoFormDiv.appendChild(todoForm);
+
+    const todoInput = document.createElement('input');
+    todoInput.setAttribute('id', 'todo');
+    todoInput.setAttribute('name', 'todo')
+    todoInput.setAttribute('type', 'text')
+
+
+    const todoLabel = document.createElement('label');
+    todoLabel.setAttribute('for', 'todo');
+    todoLabel.textContent = 'To-Do:'
+
+    const todoSubmit = document.createElement('button');
+    todoSubmit.setAttribute('id', 'submit')
+    todoSubmit.textContent = 'Submit To-Do'
+
+    todoForm.appendChild(todoLabel);
+    todoForm.appendChild(todoInput);
+    todoForm.appendChild(todoSubmit);
+}
+
+export {todoForm}
