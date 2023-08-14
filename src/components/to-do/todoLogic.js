467ed58d24cbd4798a143c8974302417638b5a76
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-14 02:09:26 -0400

    create handler for todo form submit

--- src/components/to-do/todoLogic.js
+++ src/components/to-do/todoLogic.js
@@ -1,4 +1,10 @@
 import { todoForm } from "./todoDOM";
+import { projectArr } from "../projects/projectsLogic";
+
+const todoObject = {
+    name: '',
+    priority: false
+}
 
 const todoButtonHandler = (e) => {
     const projectDiv = document.querySelector('.project')
@@ -12,4 +18,24 @@ const todoButtonHandler = (e) => {
     }
 }
 
-export {todoButtonHandler}
+const todoFormSubmit = (e) => {
+    e.preventDefault();
+    let newTodo = document.querySelector('#todo');
+    let projectID = document.querySelector('.todo-form').dataset.index;
+    todoObject.name = newTodo.value;
+    //push new todo object to the project array
+    projectArr[Number(projectID)].toDo.push(todoObject);
+
+    //clear form
+    const todoFormInput = document.querySelector('#todo')
+    todoFormInput.textContent = '';
+    todoFormInput.value = '';
+
+    //remove form
+    const project = document.querySelector('.project');
+    const todoButton = document.querySelector('.todo-btn');
+    todoButton.dataset.active = 'false'
+    project.removeChild(project.lastChild)
+}
+
+export {todoButtonHandler, todoFormSubmit}
